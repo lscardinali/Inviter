378bc625b68e39ace4ad58a63b32ff3e3e1ff62b
Lucas Salton Cardinali <lucascardinali92@gmail.com>
2019-03-19 18:31:47 -0300

    initial commit

---
+++ Inviter/Customer.swift
@@ -0,0 +1,28 @@
+//
+//  Customer.swift
+//  Inviter
+//
+//  Created by lucas.cardinali on 3/19/19.
+//  Copyright © 2019 Lucas Salton Cardinali. All rights reserved.
+//
+
+import Foundation
+
+struct Customer: Codable {
+    let name: String
+    let userId: Int
+}
+
+struct Coordinate: Codable {
+    let latitude: Double
+    let longitude: Double
+
+    func compareDistanceWith(coordinate: Coordinate) -> Double {
+        let radius = 6371e3;
+        let delta = (coordinate.longitude - longitude) * Double.pi / 180
+        let lat1 = latitude * Double.pi / 180
+        let lat2 = coordinate.latitude * Double.pi / 180
+
+        return acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(delta)) * radius
+    }
+}
